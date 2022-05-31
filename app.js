9aaf1122e3314df0c4d587294b3b7b4793bca3ff
Abhishek Maurya <abhishekmaurya2040@gmail.com>
2022-05-31 23:43:10 +0530

    Changing port

--- app.js
+++ app.js
@@ -142,6 +142,6 @@ if (port == null || port == ""){
 app.listen(port);
 
 
-app.listen(port, function() {
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
